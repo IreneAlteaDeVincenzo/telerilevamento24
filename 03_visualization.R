14ec066d88ee25057036656b398b927978c2305f
IreneAlteaDeVincenzo <162603130+IreneAlteaDeVincenzo@users.noreply.github.com>
2024-03-14 17:00:24 +0100

    Update 03_visualization.R

--- 03_visualization.R
+++ 03_visualization.R
@@ -1,10 +1,34 @@
 # Visualizzare immagini da satellite tramite imageRy su R
 
 library(terra)
-library(imageRy)
+library(imageRy) #carichiamo i dati
 
 # List of data avialable in imageRy
-im.list()
+im.list() #tutte le immagini in imagery cominciamo con im. 
 
-# Importa dati
-mato <- im.import("matogrosso_ast_2006209_lrg.jpg")
+# Importa dati da imagery
+b2<- im.import("sentinel.dolomites.b2.tif") # in questo modo andiamo ad importare l'immagine raster associata 
+
+# Plot dati
+plot(b2)
+
+#A questo punto, possiamo andare a cambiare la scala di colori. Per farlo, usiamo la funzione "colorRampPalette". scegli la scala di colori e concatenali con la funzione "c"
+clg<-colorRampPalette(c("black", "grey", "light grey"))(3) # funzione "c(x)" serve a concatenare tutti i colori insieme. Aggiungiamo "(3)" per dire in lunero di sfumature da usare, ma meglio usarne anche (100)
+plot(b2, col=clg)
+
+#Proviamo di nuovo, usando un'altra scala di colori 
+clcyan<-colorRampPalette(c("magenta", "cyan4", "cyan"))(100)
+plot(b2, col=clcyan)
+
+#Importa anche bande aggiuntive. 
+#IMPORTO BANDA VERDE
+b3<- im.import("sentinel.dolomites.b3.tif") # in questo modo andiamo ad importare l'immagine raster associata 
+plot(b3)
+
+#IMPORTO BANDA ROSSA
+b4<- im.import("sentinel.dolomites.b4.tif") # in questo modo andiamo ad importare l'immagine raster associata 
+plot(b4, col=clcyan)
+
+#IMPORTO BANDA INFRAROSSO
+b8<- im.import("sentinel.dolomites.b8.tif") # in questo modo andiamo ad importare l'immagine raster associata 
+plot(b8,col=clcyan)
