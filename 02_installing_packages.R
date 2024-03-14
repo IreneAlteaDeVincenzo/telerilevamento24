7b41956765b57be193679164d01aa9d7f7fec81d
IreneAlteaDeVincenzo <162603130+IreneAlteaDeVincenzo@users.noreply.github.com>
2024-03-14 11:56:48 +0100

    Update 02_installing_packages.R

--- 02_installing_packages.R
+++ 02_installing_packages.R
@@ -2,3 +2,6 @@
 
 # install.packages("x") è la funzione per installare i pacchetti da Cran
 install.packages("terra") 
+
+# library (x) è la funzione per testare che l'installazione sia andata a buon fine
+library(terra)
