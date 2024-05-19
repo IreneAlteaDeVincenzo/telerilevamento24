e26d24f34d8bd56965890402e8ac6a8ada581132
IreneAlteaDeVincenzo <162603130+IreneAlteaDeVincenzo@users.noreply.github.com>
2024-05-19 10:52:22 +0200

    01_beginning.R

--- 01_beginning.R
+++ 01_beginning.R
@@ -1,34 +1,48 @@
-# First R script
-#R as a calculator
-a <- 6*7
+# FIRST R SCRIPT
+
+# ------------------ Utilizzare R come una calcolatrice ------------------ 
+a <- 6 * 7  # Il simbolo <- assegna i valori (6*7) al vettore chiamato "a"
 b <- 5*8
+a+b # R eseguirà l'operazione somma, mostrandoci il risultato
 
-a+b
+# ARRAYS
 
-
-
-
-# Arrays
-
-flowers <-c(3, 6, 8, 10, 15, 18)
-flowers
+# ------------------ CREARE UN VETTORE ------------------ 
+flowers <-c(3, 6, 8, 10, 15, 18) # Questa riga di codice crea un vettore chiamato “flowers” contenente i seguenti valori: 3, 6, 8, 10, 15 e 18
+flowers # Richiamando il vettore, posso vedere i numeri di cui è composto
 
 insects <-c(0, 16, 25, 42, 61, 73)
 insects
-#ora andiamo a mettere in relazione i nostri numeri plottando flowers vs insects in un grafico
-#per fare questo esiste una funzione chiamata plot(x)
 
+# Plottare i valori "flowers vs insects" in un grafico
 plot(flowers, insects)
 
-#CHANGING PLOT PARAMETERS
+# AGGIUNTA MIA: inseriamo un titolo al grafico usando "main="
+plot(flowers, insects, main="Flowers Vs Insects") 
 
-#symbols shape
-plot(flowers, insects, pch=19)
+# ------------------ CHANGING PLOT PARAMETERS ------------------ 
 
-#symbol dimension
-plot(flowers, insects, pch=19, cex=2) #con cex=2 avrò i simboli che saranno il doppio di quelli originali. se vuoi ridurne le dimensioni utilizza cex=.5 (che è uguale a 0.5)
+# Symbols shape
+# Utilizzare l’argomento "pch" (plotting character) nella funzione plot()
+plot(flowers, insects, pch=19) # pch=19 plotta "cerchi pieni"
 
-#color
-plot(flowers, insects, pch=19, cex=2, col="chocolate1") #funzione col=x per scegliere colore plot
+# -----Altri simboli utilizzabili -----
+# pch=15 quadrato pieno 
+# pch=17 triangolo pieno
+# pch=0 quadrato vuoto etc.
 
+# Symbol dimension
+# Utilizzare l’argomento "cex" (character expansion) nella funzione plot()
+plot(flowers, insects, pch=19, cex=2) # Aumenta la dimensione dei punti x2
+plot(flowers, insects, pch=19, cex=0.5) # Riduce la dimensione dei punti di 1/2
+
+# Change color
+# Utilizzare l'argomento "col=x" per cambiare il colore del plot
+plot(flowers, insects, pch=19, cex=2, col="chocolate1") 
+
+# -----Altri colori utilizzabili -----
+# col="blue"
+# col="green"
+# col="red"
+# col="purple" etc.
 
